6a88433362ef44ddb7abe00c37bed7e3bbc8bf43
abdelrauof mohammed <abdelrauof97@gmail.com>
2017-12-26 21:15:34 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -2,14 +2,16 @@ class TicTacToe
   def initialize(board = nil)
     @board=board || Array.new(9, " ")
   end 
+  
   WIN_COMBINATIONS = [ [0,1,2], [3,4,5], [6,7,8], [0,3,6], [1,4,7], [2,5,8], [0,4,8], [2,4,7] ]
+  
   def display_board
     @board 
   end
   
   def input_to_index
     user_input = gets.strip
-    user_input
+    index = 
     
     
 end
